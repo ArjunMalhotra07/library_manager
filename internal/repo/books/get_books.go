5fc30803ef1af63581ee30334dc1b5da0c2744be
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2024-12-05 12:55:49 +0530

    ..

--- internal/repo/books/get_books.go
+++ internal/repo/books/get_books.go
@@ -4,6 +4,6 @@ import "github.com/ArjunMalhotra07/internal/model"
 
 func (r *GormBookRepo) GetAllBooks() ([]model.Book, error) {
 	var books []model.Book
-	err := r.Driver.Preload("Authorcl").Find(&books).Error
+	err := r.Driver.Preload("Author").Find(&books).Error
 	return books, err
 }
